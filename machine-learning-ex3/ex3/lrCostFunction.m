9fa6fb9902433998fccd60e82353f15731563c44
Aditya Srivastava <aditya.srivastava@berkeley.edu>
2018-08-17 20:19:19 -0700

    fixed fp

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -35,17 +35,11 @@ grad = zeros(size(theta));
 %           temp(1) = 0;   % because we don't add anything for j = 0  
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
-predictions = sigmoid(X * theta);
-
-J = ((1/m) * sum(-y' * log(predictions) - (1 - y') * log(1 - predictions))) + ((lambda/2*m) * sum(theta.^2));
-
-grad = (1/m) * X' * (predictions - y);
+htheta = sigmoid(X * theta);
+J = 1 / m * sum(-y .* log(htheta) - (1 - y) .* log(1 - htheta)) + lambda / (2 * m) * sum(theta(2:end) .^ 2);
 temp = theta;
 temp(1) = 0;
-grad = grad + ((lambda/m) * temp);
-
-
-
+grad = 1 / m * (X' * (htheta - y) + lambda * temp);
 
 
 
